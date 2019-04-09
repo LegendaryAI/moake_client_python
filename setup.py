368fe28eada6053ded1afee4f1f27975540a29c8
Barrett Jones <barretthjones@gmail.com>
2019-04-09 10:24:18 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='moake_client_python',
+    version='0.0.1',
+    description='The Moake Client for Python',
+    url='https://github.com/LegendaryAI/moake_client_python',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['python-dotenv', 'requests']
+)
